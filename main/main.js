288932dbe939c7f4b9e08bac1e93db9137666d03
“your_uLiDongpeng1 <l>
2019-07-10 20:57:28 +0800

    finish conbineItem

--- main/main.js
+++ main/main.js
@@ -19,6 +19,20 @@ function countItems(inputs) {
   );
 }
 
+function conbineItem(barcodeCounts, allItems) {
+  const result = [];
+  for (const barcodeCount of barcodeCounts) {
+    for (const item of allItems) {
+      if (barcodeCount.barcode === item.barcode) {
+        barcodeCount.item = item;
+        result.push(barcodeCount);
+        break;
+      }
+    }
+  }
+  return result;
+}
+
 function printReceipt(inputs) {
   console.log('请在此实现练习要求，并改写该行代码。');
 }
