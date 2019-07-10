b3ccbf8aad65d7086f255cf093e0b5a260fddde0
“your_uLiDongpeng1 <l>
2019-07-10 20:41:49 +0800

    finish countItems

--- main/main.js
+++ main/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+function countItems(inputs) {
+  const barcodeCounts = {};
+  const barcodeArr = [];
+  for (const barcode of inputs) {
+    if (barcodeCounts[barcode] === undefined) {
+      barcodeCounts[barcode] = 1;
+      barcodeArr.push(barcode);
+    } else {
+      barcodeCounts[barcode]++;
+    }
+  }
+  return barcodeArr.map((value) => 
+    ({
+      barcode: value,
+      count: barcodeCounts[value],
+    })
+  );
+}
+
 function printReceipt(inputs) {
   console.log('请在此实现练习要求，并改写该行代码。');
 }
